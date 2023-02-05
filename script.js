583d0066cfd7536b6e808378bca0b2a50ce5e6f1
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-02-05 12:21:19 -0500

    Created calc functions, basic operator func

---
+++ script.js
@@ -0,0 +1,52 @@
+// Calculation functions START
+
+/* Returns: Sum of two numbers
+ * args: Two numbers */
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
+/* Returns: Subtraction result of two numbers
+ * args: Two numbers */
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+
+/* Returns: Multiplication result of two numbers
+ * args: Two numbers */
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
+
+/* Returns: Division of two numbers
+ * args: Two numbers */
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+
+// Calculation functions END
+
+/* Returns: Calc function
+ * args: operator, two numbers */
+function operate(operator, numberOne, numberTwo) {
+  switch (operator) {
+    case "+":
+      console.log(add(numberOne, numberTwo));
+      break;
+
+    case "-":
+      console.log(subtract(numberOne, numberTwo));
+      break;
+
+    case "*":
+      console.log(multiply(numberOne, numberTwo));
+      break;
+
+    case "/":
+      console.log(divide(numberOne, numberTwo));
+      break;
+
+    default:
+      return;
+  }
+}
